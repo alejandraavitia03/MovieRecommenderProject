b8701e5e583b751c18c312f5d78ee8cc34768a3b
Alejandra Avitia <aleavitia03@gmail.com>
2021-03-11 00:19:27 -0800

    Update MovieGenres.cpp

--- MovieGenres.cpp
+++ MovieGenres.cpp
@@ -1,32 +1,50 @@
+
 #ifndef MovieGenres_h
 #define MovieGenres_h
 
 #include "MovieComponent.h"
+#include "Movie.h"
 #include <iterator>
-
+#include <algorithm>
+#include <list>
 class MovieGenres : public MovieComponent{
 private:
    
-    vector<Movie*> movieList;
-    vector<MovieGenres*> genreList;
+    vector<MovieComponent> movieComponents;
+    
+    string genreName;
     
 public:
-    MovieGenres() : MovieComponent() {};
-    MovieGenres(vector<string> genres, string title) : MovieComponent(genres, title) {};
-    
-    string getTitle(){ return this->title; }
-    vector<string>getGenres(){ return this->genre; }
-    
-    
-    
-    void addMovie(Movie* newMovie){
-        this->movieList.push_back(newMovie);
+    MovieGenres(string newGenreName){
+        genreName = newGenreName;
     }
-    void addGenre(MovieGenres *newGenre){
-        this->genreList.push_back(newGenre);
+    
+    string getGenreName(){ return genreName; }
+    
+    void addComponent(MovieComponent const &newMovieComponent){
+        movieComponents.push_back(newMovieComponent);
     }
-   
+    
+    void removeMovieComponent(MovieComponent const &newMovieComponent){
+        vector<MovieComponent>::iterator it;
+        it = find(movieComponents.begin(), movieComponents.end(),newMovieComponent);
+        if(it != movieComponents.end()){
+            movieComponents.erase(it);
+        }
+    }
+    /*Not working
+    MovieComponent const &getComponent(int componentIndex){
+        return (MovieComponent)movieComponents.at(componentIndex);
+    }*/
+    
+    void displayInfo(){
+        cout << "Genre Name: " << setw(8) << getGenreName();
+        //We want to cycle throulg and print any Movies or SubGenres to this Genre vector
+        for(int i  = 0; movieComponents.size(); i++){
+            movieComponents[i].displayInfo();
+        }
+    }
+    
+    
 };
 #endif /* MovieGenres_h */
-
-
